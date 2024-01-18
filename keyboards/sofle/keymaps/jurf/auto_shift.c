0095819cd42cd89a405e2b21dffac2dd72245edd
Juraj Fiala <jurf@riseup.net>
2024-01-18 17:25:04 +0100

    feat: Add auto-shift overrides

---
+++ keyboards/sofle/keymaps/jurf/auto_shift.c
@@ -0,0 +1,52 @@
+#include QMK_KEYBOARD_H
+
+#include "keymap.h"
+#include "keymap_extras/keymap_eurkey.h"
+
+bool get_custom_auto_shifted_key(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case EU_ACUT:
+            return true;
+        default:
+            return false;
+    }
+}
+
+void autoshift_press_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
+    switch (keycode) {
+        case EU_ACUT:
+            register_code16((!shifted) ? EU_ACUT : EU_CARN);
+            break;
+        case KC_DOT:
+            if (IS_LAYER_ON(U_NUM)) {
+                register_code16((!shifted) ? KC_DOT : KC_LEFT_PAREN);
+                break;
+            }
+            [[fallthrough]];
+        default:
+            if (shifted) {
+                add_weak_mods(MOD_BIT(KC_LSFT));
+            }
+            // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
+            register_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+    }
+}
+
+void autoshift_release_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
+    switch (keycode) {
+        case EU_ACUT:
+            unregister_code16((!shifted) ? EU_ACUT : EU_CARN);
+            break;
+        case KC_DOT:
+            if (IS_LAYER_ON(U_NUM)) {
+                unregister_code16((!shifted) ? KC_DOT : KC_LEFT_PAREN);
+                break;
+            }
+            [[fallthrough]];
+        default:
+            // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
+            // The IS_RETRO check isn't really necessary here, always using
+            // keycode & 0xFF would be fine.
+            unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+    }
+}
--- keyboards/sofle/keymaps/jurf/rules.mk
+++ keyboards/sofle/keymaps/jurf/rules.mk
@@ -1,28 +1,29 @@
-OLED_ENABLE = no
-OLED_DRIVER = ssd1306
 ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 MOUSEKEY_ENABLE = yes
-# VIA_ENABLE = yes
-# VIAL_ENABLE = yes
 LTO_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 CAPS_WORD_ENABLE = yes
-AUTO_SHIFT_ENABLE = yes
-# KEY_OVERRIDE_ENABLE = yes
 UNICODE_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
 
-ifeq ($(strip $(OLED_ENABLE)), yes)
-	SRC += oled/oled.c
+AUTO_SHIFT_ENABLE = yes
+ifeq ($(strip $(AUTO_SHIFT_ENABLE)), yes)
+	SRC += auto_shift.c
 endif
 
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = ws2812
-
 ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
 	SRC += rgb/rgb.c
 endif
+
+OLED_ENABLE = no
+OLED_DRIVER = ssd1306
+ifeq ($(strip $(OLED_ENABLE)), yes)
+	SRC += oled/oled.c
+endif
+
 
 # Save space
 # https://docs.qmk.fm/#/squeezing_avr?id=squeezing-the-most-out-of-avr
